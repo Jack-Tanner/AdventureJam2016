0087cbe73ebba737fd145f7db586d4daa7728c2e
Dan Foster <danfoster@fandoster.com>
2016-05-10 22:20:45 +0100

    Simple Inventory with UI + Pick up able items.
    I've made a simple inventory system that gets populated when a player
    pics something up.

---
+++ Unity/Assets/Scripts/Editor/InventoryInspector.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine.UI;
+
+[CustomEditor( typeof( Inventory ) )]
+public class InventroyInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        if( GUILayout.Button( "Space Items" ) )
+        {
+            Inventory myTarget = (Inventory)target;
+
+
+            RectTransform[] images = myTarget.gameObject.GetComponentsInChildren<RectTransform>();
+            List<RectTransform> UIImages = new List<RectTransform>();
+            for( int i = 0; i < images.Length;  ++i )
+            {
+                if( images[i].gameObject != myTarget.gameObject )
+                {
+                    UIImages.Add( images[i] );
+                }
+            }
+
+            UIImages.Sort( ( x, y ) => x.localPosition.x.CompareTo( y.localPosition.x ) );
+
+            float start = UIImages[0].localPosition.x;
+            float yPos = UIImages[0].localPosition.y;
+            for( int i = 0; i < UIImages.Count; ++i )
+            {
+                Vector3 position = UIImages[i].localPosition;
+                position.x = start + ( (float)i * myTarget.m_ItemSpacing ); 
+                position.y = yPos;
+                UIImages[i].localPosition = position;
+                UIImages[i].name = "Item " + (i + 1);
+            }
+        }
+
+        DrawDefaultInspector();
+    }
+}
