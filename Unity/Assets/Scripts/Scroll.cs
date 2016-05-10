3204a2d7709e9ad13dec4518a34e8afe034ab1d8
Jack Tanner <j.tanner92@outlook.com>
2016-05-10 21:39:26 +0100

    Added test scenes for Adam
    Added test scenes for Adam

---
+++ Unity/Assets/Scripts/Scroll.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scroll : MonoBehaviour {
+
+    public float MoveSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 position = transform.position;
+        position.x -= MoveSpeed * Time.deltaTime;
+        transform.position = position;
+
+    }
+}
