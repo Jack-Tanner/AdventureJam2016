8ebcf21549fc2ffc88d2d84417799b95e92427aa
Jack Tanner <j.tanner92@outlook.com>
2016-05-18 22:31:15 +0100

    Added background in train scene
    Added the background to the train scene and made it spawn enough to fit
    the whole train

--- Unity/Assets/Scripts/Respawner.cs
+++ Unity/Assets/Scripts/Respawner.cs
@@ -1,32 +1,53 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class Respawner : MonoBehaviour {
     private float fDestroyXPosition = -10.0f;
     private float fSpriteWidth = 0.0f;
 
     public GameObject RespawnObject = null;
-    private GameObject CloneObject = null;
+    private List<GameObject> ClonedObjects = new List<GameObject>();
+
+    private GameObject lastOne = null;
     
 	// Use this for initialization
 	void Start ()
     {
         if (RespawnObject) 
         {
-            CloneObject = Instantiate(RespawnObject);
+            SpriteRenderer originalRenderer = RespawnObject.GetComponent<SpriteRenderer>();
+            if (originalRenderer)
+            { 
+                fSpriteWidth = originalRenderer.bounds.size.x;
+            }
+
+            fDestroyXPosition = RespawnObject.transform.position.x - (fSpriteWidth * 4);
+
+
+            // Spawn 3 left
+            for (int i = 0; i < 3; i++)
+            {
+                GameObject newObject = Instantiate(RespawnObject);
+                if (newObject)
+                {
+                    newObject.transform.SetParent(transform);
+                    Vector3 pos = RespawnObject.transform.position;
+                    pos.x -= fSpriteWidth * (i + 1);
+                    newObject.transform.position = pos;
+                    ClonedObjects.Add(newObject);
+                }
+            }
+
+            // Spawn 1 right
+            GameObject CloneObject = Instantiate(RespawnObject);
             if (CloneObject) 
             {
                 CloneObject.transform.SetParent(transform);
-                SpriteRenderer originalRenderer = RespawnObject.GetComponent<SpriteRenderer>();
-                if (originalRenderer)
-                {
-                    fSpriteWidth = originalRenderer.bounds.size.x;
-
-                    Vector3 pos = RespawnObject.transform.position;
-                    pos.x += fSpriteWidth;
-                    CloneObject.transform.position = pos;
-                    fDestroyXPosition = RespawnObject.transform.position.x - fSpriteWidth;
-                }
+                Vector3 pos = RespawnObject.transform.position;
+                pos.x += fSpriteWidth;
+                CloneObject.transform.position = pos;
+                ClonedObjects.Add(CloneObject);
+                lastOne = CloneObject;
             }
         }
 	}
@@ -34,22 +55,26 @@ public class Respawner : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if( RespawnObject && CloneObject )
+        if (RespawnObject)
         {
             if (RespawnObject.transform.position.x <= fDestroyXPosition)
             {
                 Vector3 pos = RespawnObject.transform.position;
-                pos.x = CloneObject.transform.position.x + fSpriteWidth;
+                pos.x = lastOne.transform.position.x + fSpriteWidth;
                 RespawnObject.transform.position = pos;
-            }
-
-            if (CloneObject.transform.position.x <= fDestroyXPosition)
-            {
-                Vector3 pos = CloneObject.transform.position;
-                pos.x = RespawnObject.transform.position.x + fSpriteWidth;
-                CloneObject.transform.position = pos;
+                lastOne = RespawnObject;
             }
         }
-	
+
+        foreach(GameObject obj in ClonedObjects )
+        {
+            if (obj.transform.position.x <= fDestroyXPosition)
+            {
+                Vector3 pos = obj.transform.position;
+                pos.x = lastOne.transform.position.x + fSpriteWidth;
+                obj.transform.position = pos;
+                lastOne = obj;
+            }
+        }	
 	}
 }
