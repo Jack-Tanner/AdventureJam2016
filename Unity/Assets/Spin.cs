572aa1053f7f12240c37524a4bc5693811504c0b
awfindlay <adamwfindlay@gmail.com>
2016-05-19 21:42:03 +0100

    Watch out...I DID A CODE

---
+++ Unity/Assets/Spin.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+	private int spinSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+		spinSpeed = Random.Range(360,480);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate(0,0,spinSpeed *Time.deltaTime);
+
+	}
+}
