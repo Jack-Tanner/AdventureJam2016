4b7e2c2ca742fdfb9e3a517e7769d33e00f8888e
Dan Foster <danfoster@fandoster.com>
2016-05-20 23:11:42 +0100

    Walking outside, get back on train.
    Adam is a P.I.M.P.

---
+++ Unity/Assets/Scripts/Interactable Scripts/GetOnTrain.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetOnTrain : Interactable
+{
+
+    public override void ThenDo()
+    {
+        TrainJourneyManager.GetInstance().GoToTrain();
+    }
+
+    public override bool Highlightable()
+    {
+        return true;
+    }
+}
