bf1f9c11eb988fb2785c1160e14a2b3de5674e23
Jack Tanner <j.tanner92@outlook.com>
2016-05-20 10:42:51 +0100

    Added a script to scroll and delete overlays for later.

---
+++ Unity/Assets/Scripts/SceneOverlay.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneOverlay : MonoBehaviour {
+
+    public float MoveSpeed = 1.0f;
+    public float DestroyXPos = -20.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 position = gameObject.transform.position;
+        position.x -= MoveSpeed * Time.deltaTime;
+        gameObject.transform.position = position;
+
+        if (position.x <= DestroyXPos)
+        {
+            Destroy(gameObject);
+        }
+
+	}
+}
