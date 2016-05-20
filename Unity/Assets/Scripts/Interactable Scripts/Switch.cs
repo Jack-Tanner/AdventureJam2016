46581e6d0ae877f65e4967d80b6427ec513e082b
David Rogers <xestyr@gmail.com>
2016-05-20 21:22:02 +0100

    Switch. TODO: AUDIO

---
+++ Unity/Assets/Scripts/Interactable Scripts/Switch.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : Interactable
+{
+
+    public SpriteRenderer m_SpriteRenderer;
+    public Sprite m_RouteA;
+    public Sprite m_RouteB;
+
+    void Start()
+    {
+        UpdateSprite();
+    }
+
+    private void UpdateSprite()
+    {
+        if (TrainJourneyManager.GetInstance().m_bUseOtherTrack == false)
+        {
+            m_SpriteRenderer.sprite = m_RouteA;
+        }
+        else
+        {
+            m_SpriteRenderer.sprite = m_RouteB;
+        }
+    }
+
+    /// <summary>
+    /// Called when the player has walked over to the switch.
+    /// </summary>
+    public override void ThenDo()
+    {
+        TrainJourneyManager.GetInstance().m_bUseOtherTrack = !TrainJourneyManager.GetInstance().m_bUseOtherTrack;
+        UpdateSprite();
+        GetComponent<AudioSource>().Play();
+    }
+
+    /// <summary>
+    /// We want to show that the player can click on an item when it's in the world
+    /// </summary>
+    /// <returns></returns>
+    public override bool Highlightable()
+    {
+        return true;
+    }
+}
